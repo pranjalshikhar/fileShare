28d18caac1a053cebef790603f6190537ff638e3
Pranjal Shikhar Sinha <shikhar.pranjal3@gmail.com>
2021-06-27 19:02:36 +0530

    Update public.js

--- public.js
+++ public.js
@@ -15,7 +15,7 @@ const emailForm = document.querySelector("#emailForm");
 
 const toast = document.querySelector(".toast");
 
-const baseURL = "https://fi1e-share.herokuapp.com";
+const baseURL = "https://fi1e--share.herokuapp.com";
 const uploadURL = `${baseURL}/api/files`;
 const emailURL = `${baseURL}/api/files/send`;
 
